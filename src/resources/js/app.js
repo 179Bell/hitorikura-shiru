4735acaa2b8075cde44bc15a6fc784cea3608d69
179Bell <shingo.msdf@gmail.com>
2022-11-09 18:39:35 +0900

    vue,vuetify追加

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,7 +1,17 @@
 import './bootstrap';
-import { createApp, ref } from 'vue/dist/vue.esm-bundler';
+import { createApp, ref, VueElement } from 'vue/dist/vue.esm-bundler';
+import { createVuetify } from "vuetify";
+// import * as components from "vuetify/lib/components";
+// import * as directives from "vuetify/lib/directives";
 import Alpine from 'alpinejs';
+import Example from './components/Example.vue'
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+createApp({
+    components:{
+        Example
+    }
+}).mount('#app')
